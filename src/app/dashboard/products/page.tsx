da7e4eaee01d903f9b15b40507b8bfd7fc86e76e
Shubham Parida <127040084+shubham-parida01@users.noreply.github.com>
2025-09-21 20:43:14 +0530

    Update page.tsx

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,2 +1,1 @@
-// This file has been moved to src/app/dashboard-artisan/products/page.tsx
-// and is no longer needed in this location.
+export { default } from "@/app/dashboard-artisan/products/page";
